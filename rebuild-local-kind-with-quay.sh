f3020956481cdbfa5766616f220426871b37fddb
Simon Baird <sbaird@redhat.com>
2025-10-21 13:04:05 -0400

    Add script for full local kind/quay rebuild
    I'm not sure if this is generally useful, but this is what I'm using
    to test the Conforma Knative Service POC.
    
    Ref: https://issues.redhat.com/browse/EC-1528

---
+++ rebuild-local-kind-with-quay.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -euo pipefail
+
+function slow-title() {
+  sleep 2
+  echo ""
+  echo "************************************************************"
+  echo " 🐢 $1"
+  echo "************************************************************"
+  echo ""
+}
+
+slow-title "Deleting old cluster"
+kind delete cluster --name konflux
+
+slow-title "Creating cluster"
+kind create cluster --name konflux --config kind-config.yaml
+
+slow-title "Deploying dependencies"
+./deploy-deps.sh
+
+slow-title "Deploying Konflux"
+./deploy-konflux.sh
+
+slow-title "Deploying demo users"
+./deploy-test-resources.sh
+sleep 2
+
+slow-title "Deploying PAC secret"
+./deploy-pac-github-secret.sh
+
+slow-title "Deploying image controller"
+(source .env && ./deploy-image-controller.sh "$QUAY_TOKEN" "$QUAY_ORG")
+
+slow-title "Deploying Quay secret"
+./deploy-quay-push-secret.sh
+
+slow-title "Ready"
+echo "https://localhost:9443/"
